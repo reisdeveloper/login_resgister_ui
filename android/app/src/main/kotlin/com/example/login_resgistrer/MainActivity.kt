5610a9484a918b9df52b73a4afbecc5a43d26508
Jhonatan Santana <reisdeveloper10@gmail.com>
2024-04-04 21:57:49 -0300

    initial project

---
+++ android/app/src/main/kotlin/com/example/login_resgistrer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.login_resgistrer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/config/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color purple = Color(0xFF6C63FF);
+  static const Color purpleLight = Color(0xFF9292FD);
+  static const Color greyLight = Color(0xFFe6e6e6);
+  static const Color grey = Color(0xFF757575);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
