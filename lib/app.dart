066765ada3ef4b232348d58188dcac2c7d30bbb0
Jhonatan Santana <reisdeveloper10@gmail.com>
2024-04-04 22:42:53 -0300

    firebase add to the project

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:login_resgistrer/app.dart';
+import 'package:login_resgistrer/firebase_options.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const App());
+}
