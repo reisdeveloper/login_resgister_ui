be0234fd6bbd0f2230fcb693f0c42fdcdd8dc0da
Jhonatan Santana <reisdeveloper10@gmail.com>
2024-04-04 23:23:07 -0300

    add customText to the project

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:login_resgistrer/src/config/theme/app_theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,6 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.appTheme,
     );
   }
 }
--- lib/src/config/theme/app_theme.dart
+++ lib/src/config/theme/app_theme.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:login_resgistrer/src/config/constants/colors.dart';
 
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData? appTheme = ThemeData(
+    primaryColor: AppColors.purple,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    scaffoldBackgroundColor: AppColors.greyLight,
+    fontFamily: 'Roboto Slab',
+    buttonTheme: const ButtonThemeData(
+      buttonColor: AppColors.purpleLight,
+      textTheme: ButtonTextTheme.primary,
+    ),
+    iconButtonTheme: const IconButtonThemeData(
+      style: ButtonStyle(
+        iconColor: MaterialStatePropertyAll(
+          AppColors.white,
+        ),
+      ),
+    ),
+    textTheme: const TextTheme(
+      titleSmall: TextStyle(
+        color: AppColors.grey,
+      ),
+      titleMedium: TextStyle(
+        color: AppColors.black,
+      ),
+      titleLarge: TextStyle(
+        color: AppColors.black,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
